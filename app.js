c8050ce77fa248069fdc3da1f2553885d524534e
Anirban Paira <pairaanirban123@gmail.com>
2022-05-22 04:35:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8000;
+
+//EXPRESS SPECIFIC STUFF
+app.use("/static", express.static("static")); //FOR SERVING STATIC FILES
+app.use(express.urlencoded());
+
+//PUG SPECIFIC STUFF
+app.set("view engine", "pug"); //SET THE TEMPLATE ENGINE AS PUG
+app.set("views", path.join(__dirname, "views")); //SET THE VIEWS DIRECTORY
+
+//ENDPOINTS
+
+app.get("/", (req, res) => {
+  const params = {};
+  res.status(200).render("index.pug", params);
+});
+
+//START THE SERVER
+app.listen(port, () => {
+  console.log(`The application started successffully on port ${port}`);
+});
